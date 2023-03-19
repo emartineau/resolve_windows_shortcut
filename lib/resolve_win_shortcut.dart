275ccd99763ccf308fa7659f19fdf3f2aada6d04
Eric <emartineau52@gmail.com>
2023-03-19 04:55:30 -0400

    Add/Update doc comments, rename some stuff

--- lib/resolve_win_shortcut.dart
+++ lib/resolve_win_shortcut.dart
@@ -1,4 +1,6 @@
-/// Contains functions and extensions that parse Windows Shorcut (.lnk) files
+/// Contains functions and extensions that parse MS Shell Link (Windows Shorcut) files
+///
+/// Implemented based on [the open spec of Shell Links.](https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-shllink/16cb4ca1-9339-4d0c-a68d-bf1d6cc0f943)
 library resolve_win_shortcut;
 
 export 'src/resolve_win_shortcut_base.dart';
@@ -7,24 +9,26 @@ import 'dart:io';
 import 'dart:typed_data';
 
 extension DirectoryHelpers on Directory {
+  /// Determines if the directory contains any entities that
+  /// point to directories or are directories themselves
   Future<bool> hasSubDirectory() async {
     await for (final entity in list(recursive: false)) {
       if (entity is Directory ||
           _getPathExtension(entity.path) == '.lnk' &&
-              await (entity as File).resolveIfShortcut(shortcutType: FsEntity.directory) != null) {
+              await (entity as File).resolveIfShortcut(targetType: FileSystemEntityType.directory) != null) {
         return true;
       }
     }
     return false;
   }
 
-  /// Lists the contents of the directory with any
+  /// Lists the contents of the directory with any functioning links resolved
   Stream<FileSystemEntity> listWithResolvedShortcuts(
-      {FsEntity fsEntityType = FsEntity.any, bool recursive = false}) async* {
+      {FileSystemEntityType fsEntityType = FileSystemEntityType.any, bool recursive = false}) async* {
     await for (final FileSystemEntity entity in list()) {
       if (entity is File && _getPathExtension(entity.path) == 'lnk') {
         Directory resolvedDir =
-            Directory((await Future<String?>(() => entity.resolveIfShortcut(shortcutType: fsEntityType)) ?? ''));
+            Directory((await Future<String?>(() => entity.resolveIfShortcut(targetType: fsEntityType)) ?? ''));
         if (resolvedDir.path.isEmpty || !resolvedDir.existsSync()) continue;
         if (recursive) {
           yield* resolvedDir.listWithResolvedShortcuts(fsEntityType: fsEntityType, recursive: recursive);
@@ -39,7 +43,7 @@ extension DirectoryHelpers on Directory {
   }
 }
 
-// Unique Identifier for shortcut files in bytes
+/// Unique Identifier for Shell Link files in byte array form
 const shellLinkGuidHexList = [
   0x01,
   0x14,
@@ -59,26 +63,27 @@ const shellLinkGuidHexList = [
   0x46
 ];
 
-enum FsEntity { file, directory, any }
+enum FileSystemEntityType { file, directory, any }
 
 class ShortcutResolver {
-  /// Resolves the path linked by the Windows "Shortcut" (.lnk) file
-  /// [shortcutType] will only resolve if the path points to the given type
+  /// Resolves the target path linked by the Shell Link (Windows Shortcut) file
+  /// [targetType] will only resolve if the path points to the given type
   ///
   /// Throws an [ArgumentError] on failure
-  static Future<String?> resolve(Uint8List bytes, {FsEntity shortcutType = FsEntity.any}) async {
+  static Future<String?> resolveTarget(Uint8List bytes,
+      {FileSystemEntityType targetType = FileSystemEntityType.any}) async {
     // Shortcuts should be at least this long
     if (bytes.lengthInBytes < 0xff) throw ArgumentError('More data needed to attempt path resolution');
 
-    // Check if bytes 4-20 match the GUID (UUID) of MS SHELL LINK files
+    // Check if bytes 4-20 match the GUID (UUID) of MS Shell Link files
     if (!_listEquals(Uint8List.sublistView(bytes, 0x04, 0x14), shellLinkGuidHexList)) {
-      throw ArgumentError('Data is not in SHELL LINK format');
+      throw ArgumentError('Data is not in Shell Link format');
     }
 
     // Flags
     final int shellLinkFlags = bytes[0x14];
     const int fileAttributesOffset = 0x18;
-    const int hasLocationInfoMask = 0x02; // B
+    const int hasLocationInfoFlag = 0x02; // B
 
     // Attributes
     final int fileAttributes = bytes[fileAttributesOffset];
@@ -86,11 +91,12 @@ class ShortcutResolver {
     final bool linksToDir = fileAttributes & hasWorkingDirFlag < 1;
 
     // Do not attempt to resolve if the link points to the wrong entity type
-    if ((shortcutType == FsEntity.directory && !linksToDir) || (shortcutType == FsEntity.file && linksToDir)) {
-      throw ArgumentError.value('Link points to invalid shortcut type. Expected: ${shortcutType.name}');
+    if ((targetType == FileSystemEntityType.directory && !linksToDir) ||
+        (targetType == FileSystemEntityType.file && linksToDir)) {
+      throw ArgumentError.value('Link points to invalid shortcut type. Expected: ${targetType.name}');
     }
 
-    const int shellOffset = 0x4c; // 76, length of shell link header
+    const int shellOffset = 0x4c; // 76, length of the Shell Link header
     const int hasShellFlag = 0x01; // A
     const int isUnicodeFlag = 0x80; // H
 
@@ -103,24 +109,26 @@ class ShortcutResolver {
     final int fileStart = shellOffset + shellLength;
     final bool isUnicodeTarget = (shellLinkFlags & isUnicodeFlag) > 0 &&
         ByteData.sublistView(bytes).getUint32(fileStart + 0x04, Endian.little) > 28;
-    // final bool hasLocationInfo = flags & hasLocationInfoMask > 0;
+    // final bool hasLocationInfo = flags & hasLocationInfoFlag > 0;
     // get the local volume and local system values
     // const int basenameOffsetOffset = 0x10;
     // const int finalnameOffsetOffset = 0x18;
     // final int basenameOffset = rawContent[fileStart + basenameOffsetOffset] + fileStart;
     // final int finalnameOffset = rawContent[fileStart + finalnameOffsetOffset] + fileStart;
-    final String target = _parseLnkTarget(bytes, fileStart, isUnicode: isUnicodeTarget);
-    return target;
+    return _parseLnkTarget(bytes, fileStart, isUnicode: isUnicodeTarget);
   }
 }
 
 extension FileHelpers on File {
-  Future<String?> resolveIfShortcut({FsEntity shortcutType = FsEntity.any}) async =>
-      ShortcutResolver.resolve(await readAsBytes(), shortcutType: shortcutType);
+  Future<String?> resolveIfShortcut({FileSystemEntityType targetType = FileSystemEntityType.any}) async =>
+      ShortcutResolver.resolveTarget(await readAsBytes(), targetType: targetType);
 }
 
+/// Parses the target the list of bytes beginning at the given offset.
+/// For proper parsing, [isUnicode] must be true if and only if the target is in Unicode
 String _parseLnkTarget(Uint8List bytes, int offset, {bool isUnicode = false}) {
   if (!isUnicode) {
+    // Characters are in ASCII and thus only 8 bits wide
     return _getNullTerminatedString(bytes, bytes[offset + 0x10] + offset) +
         _getNullTerminatedString(bytes, bytes[offset + 0x18] + offset);
   }
@@ -157,9 +165,13 @@ String _parseLnkTarget(Uint8List bytes, int offset, {bool isUnicode = false}) {
   return as16LocalPath + as16RemainingPath;
 }
 
+/// Returns a UTF-16 string using character codes found in [bytes]
+/// starting from [offset] and ending at the first null character code.
+///
+/// The input characters are expected to be 1-byte wide.
 String _getNullTerminatedString(Uint8List bytes, int offset) {
   int length = 0;
-  // count bytes until the null character (0)
+  // Count bytes until the null character (0)
   while (true) {
     if (bytes[offset + length] == 0) {
       return String.fromCharCodes(bytes, offset, offset + length);
@@ -168,7 +180,10 @@ String _getNullTerminatedString(Uint8List bytes, int offset) {
   }
 }
 
+/// Get the extension of the given file path if it exists
 String _getPathExtension(String path) => path.length > 3 ? path.split('.').last : '';
+
+/// Compares 2 lists for equality in elements (via ==) and order
 bool _listEquals(List a, List b) {
   if (a.length != b.length) return false;
   for (int i = 0; i < a.length; ++i) {
--- test/resolve_win_shortcut_test.dart
+++ test/resolve_win_shortcut_test.dart
@@ -6,16 +6,18 @@ import 'package:test/test.dart';
 void main() {
   group('Shortcut Resolver Tests', () {
     test('ASCII file path', () async {
-      assert(await ShortcutResolver.resolve(await File('test_file.lnk').readAsBytes()) == "C:\\test\\a.txt");
+      assert(await ShortcutResolver.resolveTarget(await File('test_file.lnk').readAsBytes()) == "C:\\test\\a.txt");
     });
     test('ASCII folder path', () async {
-      assert(await ShortcutResolver.resolve(await File('test_folder.lnk').readAsBytes()) == "C:\\test");
+      assert(await ShortcutResolver.resolveTarget(await File('test_folder.lnk').readAsBytes()) == "C:\\test");
     });
     test('Unicode file path', () async {
-      assert(await ShortcutResolver.resolve(await File('test_unicode_file.lnk').readAsBytes()) == "C:\\test\\☆.txt");
+      assert(
+          await ShortcutResolver.resolveTarget(await File('test_unicode_file.lnk').readAsBytes()) == "C:\\test\\☆.txt");
     });
     test('Unicode folder path', () async {
-      assert(await ShortcutResolver.resolve(await File('test_unicode_folder.lnk').readAsBytes()) == "C:\\test\\☆");
+      assert(
+          await ShortcutResolver.resolveTarget(await File('test_unicode_folder.lnk').readAsBytes()) == "C:\\test\\☆");
     });
   });
 }
