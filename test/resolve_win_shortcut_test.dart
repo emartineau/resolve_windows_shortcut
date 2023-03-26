7bc046ab1a9850cef47a1599e10aa356c547ad83
Eric <emartineau52@gmail.com>
2023-03-25 22:11:59 -0400

    Fix unit tests to use the intended `expect` instead of `assert`, which is only for dev runtime checks. Add test cases for targetPath type

--- test/resolve_win_shortcut_test.dart
+++ test/resolve_win_shortcut_test.dart
@@ -4,18 +4,78 @@ import 'package:resolve_win_shortcut/resolve_win_shortcut.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Shortcut Resolver Tests', () {
+  // Test Shell Link paths
+  const asciiFile = 'test_file.lnk';
+  const asciiDirectory = 'test_folder.lnk';
+  const unicodeFile = 'test_unicode_file.lnk';
+  const unicodeDirectory = 'test_unicode_folder.lnk';
+
+  group('Shortcut Resolver Tests - Any Target', () {
     test('ASCII file path', () async {
-      assert(ShortcutResolver.resolveTarget(await File('test_file.lnk').readAsBytes()) == "C:\\test\\a.txt");
+      expect(ShortcutResolver.resolveTarget(await File(asciiFile).readAsBytes()), "C:\\test\\a.txt");
     });
     test('ASCII folder path', () async {
-      assert(ShortcutResolver.resolveTarget(await File('test_folder.lnk').readAsBytes()) == "C:\\test");
+      expect(ShortcutResolver.resolveTarget(await File(asciiDirectory).readAsBytes()), "C:\\test");
     });
     test('Unicode file path', () async {
-      assert(ShortcutResolver.resolveTarget(await File('test_unicode_file.lnk').readAsBytes()) == "C:\\test\\☆.txt");
+      expect(ShortcutResolver.resolveTarget(await File(unicodeFile).readAsBytes()), "C:\\test\\☆.txt");
     });
     test('Unicode folder path', () async {
-      assert(ShortcutResolver.resolveTarget(await File('test_unicode_folder.lnk').readAsBytes()) == "C:\\test\\☆");
+      expect(ShortcutResolver.resolveTarget(await File(unicodeDirectory).readAsBytes()), "C:\\test\\☆");
+    });
+  });
+
+  group('Shortcut Resolver - File Targets Only', () {
+    test('ASCII file path', () {
+      expect(
+          () async => ShortcutResolver.resolveTarget(await File(asciiFile).readAsBytes(),
+              targetType: FileSystemEntityType.file),
+          throwsArgumentError);
+    });
+    test('ASCII folder path', () async {
+      expect(
+          ShortcutResolver.resolveTarget(await File(asciiDirectory).readAsBytes(),
+              targetType: FileSystemEntityType.file),
+          "C:\\test");
+    });
+    test('Unicode file path', () {
+      expect(
+          () async => ShortcutResolver.resolveTarget(await File(unicodeFile).readAsBytes(),
+              targetType: FileSystemEntityType.file),
+          throwsArgumentError);
+    });
+    test('Unicode folder path', () async {
+      expect(
+          ShortcutResolver.resolveTarget(await File(unicodeDirectory).readAsBytes(),
+              targetType: FileSystemEntityType.file),
+          "C:\\test\\☆");
+    });
+  });
+
+  group('Shortcut Resolver - Directory Targets Only', () {
+    test('ASCII file path', () async {
+      expect(
+          ShortcutResolver.resolveTarget(await File(asciiFile).readAsBytes(),
+              targetType: FileSystemEntityType.directory),
+          "C:\\test\\a.txt");
+    });
+    test('ASCII folder path', () {
+      expect(
+          () async => ShortcutResolver.resolveTarget(await File(asciiDirectory).readAsBytes(),
+              targetType: FileSystemEntityType.directory),
+          throwsArgumentError);
+    });
+    test('Unicode file path', () async {
+      expect(
+          ShortcutResolver.resolveTarget(await File(unicodeFile).readAsBytes(),
+              targetType: FileSystemEntityType.directory),
+          "C:\\test\\☆.txt");
+    });
+    test('Unicode folder path', () {
+      expect(
+          () async => ShortcutResolver.resolveTarget(await File(unicodeDirectory).readAsBytes(),
+              targetType: FileSystemEntityType.directory),
+          throwsArgumentError);
     });
   });
 }
